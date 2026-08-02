706bbcc0b35a8ae7fa485117b3c89c3f1e54a138
alexalexandretres4-sys <alexalexandretres4@gmail.com>
2026-08-02 17:06:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import os
 import google.generativeai as genai
+from dotenv import load_dotenv
 
-print("Alex IA iniciando...")
+load_dotenv()
 
-# Aqui depois vamos colocar a chave do Gemini
-genai.configure(api_key="SUA_CHAVE_AQUI")
+chave = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=chave)
 
 modelo = genai.GenerativeModel("gemini-1.5-flash")
 
-resposta = modelo.generate_content("Olá Gemini, eu sou a Alex IA")
+resposta = modelo.generate_content("Olá Gemini, eu sou a Alex IA. Me apresente.")
 
 print(resposta.text)
