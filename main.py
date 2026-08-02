2586fb9d587d9af4507d6a092fe0d1c4a150b6de
alexalexandretres4-sys <alexalexandretres4@gmail.com>
2026-08-02 16:24:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("Olá! Eu sou a Alex IA.")
-print("Meu projeto está funcionando!")
+import google.generativeai as genai
+
+print("Alex IA iniciando...")
+
+# Aqui depois vamos colocar a chave do Gemini
+genai.configure(api_key="SUA_CHAVE_AQUI")
+
+modelo = genai.GenerativeModel("gemini-1.5-flash")
+
+resposta = modelo.generate_content("Olá Gemini, eu sou a Alex IA")
+
+print(resposta.text)
